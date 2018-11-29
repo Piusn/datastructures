5d1fffa68bedb8f352ba234c178599aefe4c699b
Pius <piusnjoka@gmail.com>
2018-11-29 10:45:03 +0300

    Initial commit

---
+++ src/DataStructures.CommonLibrary/LinkedList/LinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.CommonLibrary.LinkedList
+{
+    public class LinkedList
+    {
+        public LinkedListNode Head { get; set; }
+
+        //add to head
+        public void AddToHead(int data)
+        {
+            LinkedListNode node = new LinkedListNode(data);
+
+            // 1 -> 2  -> 3 - NULL
+
+            if (Head == null) //this is the first node
+            {
+                Head = new LinkedListNode(data);
+                return;
+            }
+
+            node.Next = Head;
+            this.Head = node;
+        }
+
+        //add to tail
+        public void AddToTail(int data)
+        {
+            //1 -> 2 -> 3 - NULL
+            LinkedListNode node = new LinkedListNode(data);
+
+            if (Head == null)
+            {
+                Head = node;
+                return;
+            }
+
+            var current = Head;
+
+            while (current != null)
+            {
+                if (current.Next == null)
+                {
+                    current.Next = new LinkedListNode(data);
+                    break;
+                }
+
+                current = current.Next;
+            }
+        }
+
+        //add to middle
+
+        //search
+
+        //delete
+
+    }
+}
---
+++ src/DataStructures.CommonLibrary/LinkedList/LinkedListNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.CommonLibrary.LinkedList
+{
+    /// <summary>
+    /// This is linked list node
+    /// LinkedList node contains a data property and link to the next node
+    /// </summary>
+    public class LinkedListNode
+    {
+        /// <summary>
+        /// Data property
+        /// </summary>
+        public int Data { get; set; }
+
+        /// <summary>
+        /// Next node 
+        /// </summary>
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode(int data)
+        {
+            this.Data = data;
+        }
+    }
+}
